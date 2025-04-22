994cedaa44322e37d8fd8be280db1cdc665264e3
CHICKEN <160640935+forever-fish-lover@users.noreply.github.com>
2025-04-22 09:41:23 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true,
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './dist/index.html',
+        }),
+    ],
+    devServer: {
+        static: './dist',
+        open: true,
+    }
+};
